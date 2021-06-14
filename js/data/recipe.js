9a8443493b5f15e30f6e30d0045d97c629009b58
Kaemy <fanny.perriguey@gmx.com>
2021-06-14 12:32:11 +0200

    add recipeslist byName

--- js/data/recipe.js
+++ js/data/recipe.js
@@ -1,29 +1,40 @@
 "use strict";
 
 export class Recipe {
-    constructor(
-      id,
-      name,
-      servings,
-      ingredients,
-      time,
-      description,
-      appliance,
-      ustensils
-    ) {
-      this.id = id;
-      this.name = name;
-      this.servings = servings;
-      this.ingredients = ingredients;
-      this.time = time;
-      this.description = description;
-      this.appliance = appliance;
-      this.ustensils = ustensils;
-    }
+  constructor(
+    id,
+    name,
+    servings,
+    ingredients,
+    time,
+    description,
+    appliance,
+    ustensils
+  ) {
+    this.id = id;
+    this.name = name;
+    this.servings = servings;
+    this.ingredients = ingredients;
+    this.time = time;
+    this.description = description;
+    this.appliance = appliance;
+    this.ustensils = ustensils;
   }
+}
 
-  export class RecipesList {
-    constructor(recipes) {
-      this.recipes = recipes;
-    }
+export class RecipesList {
+  constructor(recipes) {
+    this.recipes = recipes;
+  }
+  
+  byName() {
+    return this.recipes.sort((r1, r2) => {
+      const name1 = r1.name.toLowerCase();
+      const name2 = r2.name.toLowerCase();
+
+      if (name1 > name2) return 1;
+      if (name1 < name2) return -1;
+      return 0;
+    });
+  }
 } 
