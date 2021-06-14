dfb0b13d63674586ee66a67d6a3300b5cbc61b17
Kaemy <fanny.perriguey@gmx.com>
2021-06-14 22:43:52 +0200

    Add method for renderCards

--- js/main.js
+++ js/main.js
@@ -2,26 +2,12 @@
 
 import { RECIPES } from "./data/recipesData.js";
 import { DataFetcher } from "./data/dataFetcher.js";
+import { PageManager } from "./components/pageManager.js";
 
 const dataFetcher = new DataFetcher(RECIPES);
 const recipesList = dataFetcher.getRecipesList();
 
 recipesList.sortByName();
 
-console.log("--- INGREDIENTS ---");
-
-for (let ingredient of recipesList.sortedIngredients) {
-  console.log(ingredient);
-}
-
-console.log("--- APPLIANCES ---");
-
-for (let appliance of recipesList.sortedAppliances) {
-  console.log(appliance);
-}
-
-console.log("--- USTENSILS ---");
-
-for (let ustensil of recipesList.sortedUstensils) {
-  console.log(ustensil);
-}
+const pageManager = new PageManager(recipesList);
+pageManager.render();
